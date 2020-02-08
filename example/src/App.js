ad3024e0eced7d72cb3cee8b4f6b2532c792550d
justinbutt3r <justinbutt3r@gmail.com>
2020-02-08 14:32:48 +0200

    inital?

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from '@justinbutt3r/focal-image'
+import ExampleComponent from '@justinbutt3r/focal-image';
 
 export default class App extends Component {
-  render () {
-    return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
-    )
+  render() {
+    return <ExampleComponent text="Modern React component module" />;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import styles from './styles.css'
+import './styles.scss';
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+const Image = styled.div`
+  background: transparent no-repeat;
+  background-size: cover;
+  background-position: ${props => props.x}% ${props => props.y}%;
+  background-image: url('${props => props.src.mobile}');
+  
+  @media (min-width: 768px) {
+    background-image: url('${props =>
+      props.useSmall ? props.src.mobile : props.src.tablet}');
+  }  
+  @media (min-width: 1280px) {
+    background-image: url('${props =>
+      props.useLarge
+        ? props.src.desktop
+        : props.useSmall
+        ? props.src.mobile
+        : props.src.tablet}');
   }
+`;
 
-  render() {
-    const {
-      text
-    } = this.props
+const FocalImage = ({ className, src, x, y, alt, useLarge, useSmall }) => {
+  // if (!src) {
+  //   return null;
+  // }
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+  return (
+    <Image
+      className={`focal-image ${className || ''}`}
+      src={src}
+      x={x}
+      y={y}
+      useLarge={useLarge}
+      useSmall={useSmall}
+    >
+      <img src={src.mobile} alt={alt} />
+    </Image>
+  );
+};
+
+FocalImage.propTypes = {
+  className: PropTypes.string,
+  src: PropTypes.shape({
+    desktop: PropTypes.string,
+    mobile: PropTypes.string,
+    tablet: PropTypes.string,
+  }),
+  x: PropTypes.number,
+  y: PropTypes.number,
+  alt: PropTypes.string,
+  useLarge: PropTypes.bool,
+  useSmall: PropTypes.bool,
+};
+
+FocalImage.defaultProps = {
+  src: {},
+};
+export default FocalImage;
